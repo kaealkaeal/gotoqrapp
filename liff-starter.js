3c34882696ab0381ae7c8c5f9d9bf74a7ba7e084
Amonrat Loylertlar <kaealkaeal159@hotmail.com>
2019-02-07 18:29:02 +0700

    commit

--- liff-starter.js
+++ liff-starter.js
@@ -5,8 +5,7 @@ window.onload = function (e) {
 };
 
 function initializeApp(data) {
-    document.getElementById('languagefield').textContent = data.language;
-    document.getElementById('viewtypefield').textContent = data.context.viewType;
+    
     document.getElementById('useridfield').textContent = data.context.userId;
     
     
@@ -20,20 +19,17 @@ function initializeApp(data) {
 
     
 
-    // get profile call
-    document.getElementById('getprofilebutton').addEventListener('click', function () {
-        liff.getProfile().then(function (profile) {
-            document.getElementById('useridprofilefield').textContent = profile.userId;
-          
-            toggleProfileData();
-        }).catch(function (error) {
-            window.alert("Error getting profile: " + error);
-        });
-    });
-}
-
+  
 
 
 function toggleProfileData() {
     toggleElement('profileinfo');
+}
+function toggleElement(elementId) {
+    const elem = document.getElementById(elementId);
+    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
+        elem.style.display = "none";
+    } else {
+        elem.style.display = "block";
+    }
 }
