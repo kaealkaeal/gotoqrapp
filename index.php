845ed3930b2fb375307641ed824e94403df2d15a
Amonrat Loylertlar <kaealkaeal159@hotmail.com>
2019-02-07 18:09:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,18 +10,25 @@
 $(window).on('load', function(){
       
     // Initializes a LIFF app
-      liff.init(function (data) {});
+      liff.init(function (data) {
+        initializeApp(data);
+      });
     });
-	liff.getProfile().then(function (profile) {
-            const = profile.userId;
-            document.getElementById('profileid').textContent = profile.userId;
-            
-        }).catch(function (error) {
-            window.alert("Error getting profile: " + error);
-        });
+	
+function initializeApp(data) {
     
+    document.getElementById('profileid').textContent = data.context.userId;
+
+    // closeWindow call
+    document.getElementById('closewindowbutton').addEventListener('click', function () {
+        liff.closeWindow();
+    });
+  
+
+}    
 
 </script>
 <div id="profileid"></div>
+<button id="closewindowbutton">Close Window</button>
 </body>
 </html>
