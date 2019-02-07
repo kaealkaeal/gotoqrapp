ec15acc9844295692339fff979ab38c6e81c7f60
Amonrat Loylertlar <kaealkaeal159@hotmail.com>
2019-02-07 18:59:12 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -22,23 +22,10 @@ function initializeApp(data) {
             window.location = 'class://scan?userId=' + id;
            // document.getElementById('useridprofilefield').textContent = profile.userId;
           
-            toggleProfileData();
-        }).catch(function (error) {
-            window.alert("Error getting profile: " + error);
-        });
+            
+        })
     
 }
 
 
 
-function toggleProfileData() {
-    toggleElement('profileinfo');
-}
-function toggleElement(elementId) {
-    const elem = document.getElementById(elementId);
-    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
-        elem.style.display = "none";
-    } else {
-        elem.style.display = "block";
-    }
-}
